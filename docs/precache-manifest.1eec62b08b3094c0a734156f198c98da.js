aac759c7f84527fdf3252efe83a0aace8e18dc1e
谷山雄基 <cmt.0801nonki@gmail.com>
2019-12-15 17:28:21 +0900

    PWA test #1

---
+++ docs/precache-manifest.1eec62b08b3094c0a734156f198c98da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "909d70e8358af43d9339",
+    "url": "css/about.d0dbadbf.css"
+  },
+  {
+    "revision": "909d70e8358af43d9339",
+    "url": "js/about.97faa60b.js"
+  },
+  {
+    "revision": "1741d4412ac0315f58fb",
+    "url": "css/app.533405dc.css"
+  },
+  {
+    "revision": "1741d4412ac0315f58fb",
+    "url": "js/app.d8950092.js"
+  },
+  {
+    "revision": "b87dd0a7fbaa8fb69525",
+    "url": "js/chunk-vendors.896c21ca.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3f9b075d04ecdcc185112c71182100f7",
+    "url": "index.html"
+  }
+];
