35fe269d26f2ce69151f2d74de32158ef4d9351c
taniyuu <taniyuu@users.noreply.github.com>
2020-04-12 10:25:36 +0000

    deploy: feb058908011c231c1d87c49649f737240e50d09

---
+++ precache-manifest.d4783a6c0e5be4fca113cd40ee09720e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f163809cecd46ebcf8c",
+    "url": "css/about.5a64fae3.css"
+  },
+  {
+    "revision": "7f163809cecd46ebcf8c",
+    "url": "js/about.571e54ba.js"
+  },
+  {
+    "revision": "fb96271743428c1e3acc",
+    "url": "css/app.533405dc.css"
+  },
+  {
+    "revision": "fb96271743428c1e3acc",
+    "url": "js/app.c4e70d13.js"
+  },
+  {
+    "revision": "2e0c7de6b99ffb100f79",
+    "url": "js/chunk-vendors.ee3e6a58.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4a487a5200d6e5f34c41a222fa53e09e",
+    "url": "index.html"
+  }
+];
