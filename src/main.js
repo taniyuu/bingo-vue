83a1285bc18ad702e2776f8c2fcc9a00d64a3c8b
谷山雄基 <cmt.0801nonki@gmail.com>
2020-04-19 17:53:45 +0900

    test meta

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMeta from 'vue-meta'
 
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false
+
+Vue.use(VueMeta, {
+  // optional pluginOptions
+  refreshOnceOnNavigation: true
+})
 
 new Vue({
   router,
