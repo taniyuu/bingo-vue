44592f9f2b71a013639edb8b953efe3552b1e227
taniyuu <cmt.0801nonki@gmail.com>
2020-11-21 11:08:25 +0900

    🐛 : upgrade package(fix security issue) (#6)
    * package upgrade
    
    * chore
    
    * yarn ->npm

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+//const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
-    publicPath: '/',
-    outputDir: "docs",
-    configureWebpack: () => {
-        if (process.env.NODE_ENV === 'production') {
-            return {
-                plugins: [
-                    new PrerenderSPAPlugin({
-                        staticDir: path.join(__dirname, 'docs'),
-                        routes: [ '/', '/condition' ],
-                        /*renderer: new Renderer({
+  publicPath: "/",
+  outputDir: "docs",
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new PrerenderSPAPlugin({
+            staticDir: path.join(__dirname, "docs"),
+            routes: ["/", "/condition"],
+            /*renderer: new Renderer({
                             // document.dispatchEvent(new Event('custom-render-trigger'))
                             // されるまで読み込みを待つ
                             renderAfterDocumentEvent: 'custom-render-trigger'
                         })*/
-                    })
-                ]
-            }
-        }
+          }),
+        ],
+      };
     }
-}
+  },
+};
