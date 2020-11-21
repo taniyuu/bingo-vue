4ef63cdba2094eda7f2d86d8ada43ee221c14a16
taniyuu <cmt.0801nonki@gmail.com>
2020-11-21 11:36:47 +0900

    lint rule

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,30 +3,37 @@ import { register } from "register-service-worker";
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
+      // eslint-disable-next-line no-console
       console.log(
         "App is being served from cache by a service worker.\n" +
           "For more details, visit https://goo.gl/AFskqB"
       );
     },
     registered() {
+      // eslint-disable-next-line no-console
       console.log("Service worker has been registered.");
     },
     cached() {
+      // eslint-disable-next-line no-console
       console.log("Content has been cached for offline use.");
     },
     updatefound() {
+      // eslint-disable-next-line no-console
       console.log("New content is downloading.");
     },
     updated() {
+      // eslint-disable-next-line no-console
       console.log("New content is available; please refresh.");
     },
     offline() {
+      // eslint-disable-next-line no-console
       console.log(
         "No internet connection found. App is running in offline mode."
       );
     },
     error(error) {
+      // eslint-disable-next-line no-console
       console.error("Error during service worker registration:", error);
-    }
+    },
   });
 }
